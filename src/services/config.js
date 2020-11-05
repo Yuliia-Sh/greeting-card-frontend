8a3a4f27b4595a5f98552a482da13aa6d6b4a833
Yuliia_Sh <yshinkaren@gmail.com>
2020-11-05 19:34:25 +0200

    test env

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
-const apiUrl = process.env.REST_API + '/api/v1'
+const apiUrl = 'https://greetteam-env.eba-zmednwgx.us-east-2.elasticbeanstalk.com/api/v1'
 const greetingUrl = 'https://main.d177anvw131z2v.amplifyapp.com/'
 
 const config = {
