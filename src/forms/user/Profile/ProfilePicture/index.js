2b27ec7210fa9356068c563524788dd29df2ecb9
Yuliia_Sh <yshinkaren@gmail.com>
2020-11-16 15:08:42 +0200

    added preview of profile image

--- src/forms/user/Profile/ProfilePicture/index.js
+++ src/forms/user/Profile/ProfilePicture/index.js
@@ -1,37 +1,60 @@
 import React from "react";
-import './style.css';
+import "./style.css";
 
 import editImg from "../../../../assets/images/edit.jpg";
 import userImg from "../../../../assets/images/user.png";
 
-export default function ProfilePicture(props) {
-  return (
-    <div className="profile-picture__row">
-      <div className="profile-picture-change__column">
-        <label
-          htmlFor="profile-picture-add"
-          className="file-adder"
-          name="images"
-        >
-          <img
-            src={editImg}
-            alt=""
-            className="profile-type-icon"
-          />
+class ProfilePicture extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+      imageProfile: props.imageProfile ? props.imageProfile : ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-          <input
-            type="file"
-            id="profile-picture-add"
-            name="image-files"
-            accept="image/*"
-            className="files-input"
-          />
-        </label>
-        <button></button>
+  handleChange(event) {
+    this.setState({
+      file: URL.createObjectURL(event.target.files[0])
+    })
+  }
+
+  showPicture() {
+      if (this.state.imageProfile.length > 0) {
+        return <img src={this.state.imageProfile} alt="" />
+      } else if (this.state.file) {
+        return <img src={this.state.file} alt=""/>  
+      } else {
+        return <img src={userImg} alt="" className="no-picture" />  
+      }
+  }
+  
+  render() {
+
+    return (
+      <div className="profile-picture__row">
+        <div className="profile-picture-change__column">
+          <label htmlFor="profile-picture-add" name="images">
+            <img src={editImg} alt="" className="profile-type-icon" />
+
+            <input
+              type="file"
+              id="profile-picture-add"
+              name="image-files"
+              accept="image/*"
+              className="files-input"
+              onChange={this.handleChange}
+            />
+          </label>
+          <button></button>
+        </div>
+        <div className="profile-picture__picture none">
+          {this.showPicture()}
+        </div>
       </div>
-      <div className="profile-picture__picture none">
-        <img src={userImg} alt="" className="no-picture" />
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default ProfilePicture;
